6f8168e80ecc2b0c5ef20adbbeba0f2776215928
Imam Hanafi <52751983+imamhanafi1998@users.noreply.github.com>
2023-05-23 08:55:33 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Header from "./components/Header";
+import Note from "./components/Note";
+// import Footer from "./components/Footer";
+import Detail from "./components/Detail";
+import Edit from "./components/Edit";
+import Create from "./components/Create";
+import Delete from './components/Delete'
+import history from './components/History'
+import { Router, Route, Switch } from "react-router-dom";
+
+import socketIO from 'socket.io-client';
+const socket = socketIO.connect('http://realtime-chat-backend-v2.snap-gw.ramaoding.com/');
+
+function App() {
+  return (
+    <Router history={history}>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Note} />
+          <Route exact path="/create" component={Create} />
+          <Route path="/detail/:id" component={Detail} />
+          <Route path="/edit/:id" component={Edit} />
+          <Route path="/delete/:id" component={Delete} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
